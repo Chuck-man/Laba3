ce62cd227d74f2c8f447e3eb15fc18e9c8311b35
Chuck-man <sgold2398@gmail.com>
2022-12-14 14:10:35 +0400

    Окончательный вид

--- main_window.py
+++ main_window.py
@@ -17,7 +17,6 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtGui import QPixmap
 from iterator import Iterator
-from scraping import parser
 from copying_dataset import copying_dataset
 
 
@@ -29,20 +28,17 @@ class Example(QWidget):
 
     def initUI(self):
         
-        self.btn_0 = QPushButton("Создать датасет", self)
-        self.btn_0.move(50, 20)
-        self.btn_0.clicked.connect(self.showDialog_0)
         self.btn_1 = QPushButton("Создать файл анотацию исходного датасета", self)
-        self.btn_1.move(50, 60)
+        self.btn_1.move(50, 20)
         self.btn_1.clicked.connect(self.showDialog_1)
         self.btn_2 = QPushButton("Создание датасета с другой организацией файлов", self)
-        self.btn_2.move(50, 100)
+        self.btn_2.move(50, 60)
         self.btn_2.clicked.connect(self.showDialog_2)
         self.btn_3 = QPushButton("Создание копии датасета", self)
-        self.btn_3.move(50, 140)
+        self.btn_3.move(50, 100)
         self.btn_3.clicked.connect(self.showDialog_3)
         self.btn_4 = QPushButton("Просмотр изображений", self)
-        self.btn_4.move(50, 180)
+        self.btn_4.move(50, 140)
         self.btn_4.clicked.connect(self.showDialog_4)
         self.label = QLabel(self)
         self.btn_6 = QPushButton("Далее", self)
@@ -54,12 +50,6 @@ class Example(QWidget):
         self.setWindowTitle("lab3")
         self.show()
         self.folderpath = QFileDialog.getExistingDirectory(self, 'Select Folder')
-
-    def showDialog_0(self):
-        text_1, ok_ = QInputDialog.getText(self, "Input Dialog", "Какие картинки скачать?")
-        if ok_:
-
-            self.create_image = parser(str(text_1))
     
     def showDialog_1(self):
 
