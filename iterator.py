2f4e9f672fb9085de03a86c53bfc1fc010e2d53f
Chuck-man <sgold2398@gmail.com>
2022-12-14 14:10:05 +0400

    Доработка итератора

--- iterator.py
+++ iterator.py
@@ -3,6 +3,7 @@ import csv
 class Iterator:
 
     """Класс итератор - задаётся итерируемый объект и датасет"""
+    
     def __init__(self, label: str, file: str) -> None:
         self.label = label
         self.file = file
@@ -11,7 +12,8 @@ class Iterator:
         with open(self.file) as reading_file:
             file_reader = csv.reader(reading_file, delimiter=";")
             for i in file_reader:
-                self.data.append(i[0])
+                if i[2] == self.label:
+                    self.data.append(i[0])
             self.full = len(self.data)
     
     def __iter__(self):
@@ -26,7 +28,7 @@ class Iterator:
             raise StopIteration
 
 def main():
-    instance = Iterator("cat", "dataset_csv.csv")
+    instance = Iterator("dog", "dataset_csv.csv")
     print(next(instance))
     print(next(instance))
     print(next(instance))
